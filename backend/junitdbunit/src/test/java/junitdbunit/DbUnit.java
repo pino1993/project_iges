462eb859c2373e6a0b57001a74938f31b6d0dd55
Andrea Valenti <valentiandrea1993@gmail.com>
2018-01-04 16:24:01 +0100

    Modify unit test

--- backend/junitdbunit/src/test/java/junitdbunit/DbUnit.java
+++ backend/junitdbunit/src/test/java/junitdbunit/DbUnit.java
@@ -1,19 +1,31 @@
 package junitdbunit;
 
+import java.io.File;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.dbunit.Assertion;
 
 import org.dbunit.DBTestCase;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
 import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.dbunit.operation.DatabaseOperation;
 import org.junit.Test;
 
 public class DbUnit extends DBTestCase {
-
+        
+        private IDatabaseTester databaseTester;
+        
 	public DbUnit(String name) {
 		super(name);
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
@@ -33,12 +45,29 @@ public class DbUnit extends DBTestCase {
 	protected DatabaseOperation getTearDownOperation() throws Exception {
 		return DatabaseOperation.NONE;
 	}
-
+        
 	@Test
-	public void testById() {
+	public void testById() throws DataSetException, MalformedURLException, Exception {
+                 // Fetch database data after executing your code
+        IDataSet databaseDataSet = null;
+       
+            try {
+                databaseDataSet = getConnection().createDataSet();
+                
+            } catch (Exception ex) {
+                Logger.getLogger(DbUnit.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        ITable actualTable = databaseDataSet.getTable("utenti");
+        String emailActual = (String) actualTable.getValue(0, "Email_Utente");
+        
 
-		int userId = 1;// get user id from database
-		assertThat(1, is(userId));
+        // Load expected data from an XML dataset
+        IDataSet expectedDataSet = getDataSet();
+        ITable expectedTable = expectedDataSet.getTable("utenti");
+        String emailExpected = (String) expectedTable.getValue(0, "Email_Utente");
+        // Assert actual database table match expected table
+        //Assertion.assertEquals(expectedTable, actualTable);
+            assertEquals(emailExpected, emailActual);
 	}
 
 }
