e0f1f33750b9e6a4cfaec637d5a6f1dd1ba933ab
Andrea Valenti <valentiandrea1993@gmail.com>
2018-01-04 15:29:08 +0100

    Add Example DBunit

--- backend/PrezziPazzi/src/test/java/com/prezzipazzi/test/NewEmptyJUnitTest.java
+++ backend/PrezziPazzi/src/test/java/com/prezzipazzi/test/NewEmptyJUnitTest.java
@@ -7,43 +7,39 @@ package com.prezzipazzi.test;
 
 import com.prezzipazzi.autenticazione.AuthException;
 import com.prezzipazzi.bean.Utente;
-import com.prezzipazzi.database.Database;
 import com.prezzipazzi.manager.ManagerAutenticazione;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 import org.dbunit.DatabaseTestCase;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author ruggerotammaro
  */
 public class NewEmptyJUnitTest extends DatabaseTestCase  {
-     private FlatXmlDataSet loadedDataSet;
-	private Connection jdbcConnection;
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
-
+    private FlatXmlDataSet loadedDataSet;
+    private Connection jdbcConnection;
+    
+    @Resource(lookup = "jdbc/DbPrezziPazzi")
+    private static DataSource ds;
+    
     @Override
     protected IDatabaseConnection getConnection() throws Exception {
-       Class.forName("com.mysql.jdbc.Driver");
-        jdbcConnection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/DbPrezziPazzi", "root", "");
-		return new DatabaseConnection(jdbcConnection);
+       try {
+            return (IDatabaseConnection) ds.getConnection();
+        } catch (NullPointerException e) {
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/DbPrezziPazzi?autoReconnect=true&useSSL=false", "root", "root");
+            //Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/DbPrezziPazzi?autoReconnect=true&useSSL=false", "root", "");
+            return (IDatabaseConnection) myConn;
+        }
     }
 
     @Override
---
+++ backend/junitdbunit/src/test/java/junitdbunit/DbUnit.java
@@ -0,0 +1,44 @@
+package junitdbunit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.FileInputStream;
+
+import org.dbunit.DBTestCase;
+import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.Test;
+
+public class DbUnit extends DBTestCase {
+
+	public DbUnit(String name) {
+		super(name);
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost:3306/dbprezzipazzi");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "root");
+	}
+
+	protected IDataSet getDataSet() throws Exception {
+		return new FlatXmlDataSetBuilder().build(new FileInputStream("user.xml"));
+	}
+
+	protected DatabaseOperation getSetUpOperation() throws Exception {
+		return DatabaseOperation.REFRESH;
+	}
+
+	protected DatabaseOperation getTearDownOperation() throws Exception {
+		return DatabaseOperation.NONE;
+	}
+
+	@Test
+	public void testById() {
+
+		int userId = 1;// get user id from database
+		assertThat(1, is(userId));
+	}
+
+}
